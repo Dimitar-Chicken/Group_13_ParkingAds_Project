56de30b7a9504aec4b4252adeb2011a3d7d08aa9
Dimitar-Chicken <dimitar.pilyakov@gmail.com>
2020-12-08 19:29:23 +0100

    Adding ad_service.js

---
+++ ad_service.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const amqp = require('amqplib/callback_api');
+const axios = require('axios');
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+    var queue = 'rpc_queue';
+
+    channel.assertQueue(queue, {
+      durable: false
+    });
+    channel.prefetch(1);
+    console.log(' [x] Awaiting RPC requests');
+    channel.consume(queue, function reply(msg) {
+        if(msg.content.toString() == 'getAd') {
+            axios.get('http://psuaddservice.fenris.ucn.dk/').then(response => {
+                console.log(' [.] Sending %s', response.data);
+                channel.sendToQueue(msg.properties.replyTo, Buffer.from(response.data), {
+                    correlationId: msg.properties.correlationId
+                });
+            }).catch(error => {
+                console.log(error);
+            });
+        }
+        channel.ack(msg);
+    });
+  });
+});
