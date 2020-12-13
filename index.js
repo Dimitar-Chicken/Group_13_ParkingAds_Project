c0b3fa5407793071ed1ebc2755825486b85578ed
Dimitar-Chicken <dimitar.pilyakov@gmail.com>
2020-12-13 15:03:20 +0100

    Adding main index.js for easier running.

---
+++ index.js
@@ -0,0 +1,11 @@
+//TODO: Replace with actual index.
+
+console.log("Launching Express server...");
+const server = require("./server");
+
+console.log("Launching adService...");
+const adService = require("./adService");
+
+//console.log("Launching parkingService...");
+//const parkingServer = require("./parkingService");
+
